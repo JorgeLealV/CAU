d1c696df5f1d33496d7758ecf2c087bd62988138
Jorge <jlealster@gmail.com>
2020-03-18 12:56:17 -0600

    Modificacion de vinicio consulta1

--- controler/Consulta1.java
+++ controler/Consulta1.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Se modifica para en develop con Vinicio
+ * 
  */
 package com.sfp.controler;
 import com.sfp.cau.CalculaTiempo;
